fffcb7bb0a0c96fb845f8c8b5dc26b4a747d74a4
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-03-25 19:53:30 +0100

    Add: dropout and batch normalization

--- src/model.py
+++ src/model.py
@@ -7,17 +7,21 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32) 
 
         self.fc1 = nn.Linear(32 * 8 * 8, 128)
         self.fc2 = nn.Linear(128, 10)
 
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.bn1(self.conv1(x))) 
         x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
+
+        x = F.relu(self.bn2(self.conv2(x)))
         x = F.max_pool2d(x, 2)
 
         x = torch.flatten(x, 1)
